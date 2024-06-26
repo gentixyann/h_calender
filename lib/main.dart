8138c5d5a541e32b766bf389577662abeb6a4bd7
Gento <>
2024-06-26 17:48:01 +0900

    カレンダーを日本語化

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:h_calender/router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
-  runApp(const MyApp());
+  MyApp.run();
 }
 
 class MyApp extends StatelessWidget {
@@ -12,12 +14,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter Demo',
+      title: '性活カレンダー',
       routerConfig: router,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
     );
+  }
+
+  static Future<void> run() async {
+    await initializeDateFormatting('ja_JP').then((_) {
+      WidgetsFlutterBinding.ensureInitialized();
+      runApp(const ProviderScope(
+        child: MyApp(),
+      ));
+    });
   }
 }
--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ホームだよ'),
+    return Scaffold(
+      body: TableCalendar(
+        firstDay: DateTime.utc(2010, 10, 16),
+        lastDay: DateTime.utc(2030, 3, 14),
+        focusedDay: DateTime.now(),
+        locale: 'ja_JP',
       ),
     );
   }
