376b8aa36be524d24b7ff423f4b018707a5c65db
Gento <a551.return@gmail.com>
2024-09-12 14:40:33 +0900

    bottom navのためにMainScaffoldを作成

--- lib/router.dart
+++ lib/router.dart
@@ -4,10 +4,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:h_calender/views/pages/day_page.dart';
 import 'package:h_calender/views/pages/home_page.dart';
+import 'package:h_calender/views/pages/widgets/main_scaffold.dart';
 
 part 'router.g.dart';
 
 final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> shellNavigatorKey = GlobalKey<NavigatorState>();
 
 final routerProvider = Provider((ref) {
   return GoRouter(
@@ -32,15 +34,16 @@ class HomeRoute extends GoRouteData {
   Widget build(BuildContext context, GoRouterState state) => const HomePage();
 }
 
-// final router = GoRouter(initialLocation: '/', routes: [
-//   GoRoute(
-//       path: '/',
-//       builder: (BuildContext context, GoRouterState? state) => const HomePage(),
-//       routes: [
-//         GoRoute(
-//           path: 'day',
-//           builder: (BuildContext context, GoRouterState? state) =>
-//               const DayPage(),
-//         ),
-//       ]),
-// ]);
+class MainRouteData extends ShellRouteData {
+  const MainRouteData();
+
+  static final $navigatorKey = shellNavigatorKey;
+
+  @override
+  Widget builder(BuildContext context, GoRouterState state, Widget navigator) {
+    return MainScaffold(
+      body: navigator,
+      fullPath: state.fullPath,
+    );
+  }
+}
---
+++ lib/views/pages/widgets/main_scaffold.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainScaffold extends StatelessWidget {
+  const MainScaffold({
+    super.key,
+    required this.body,
+    required this.fullPath,
+  });
+
+  final Widget body;
+  final String? fullPath;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
