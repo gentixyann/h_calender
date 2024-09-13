f7c5e51620e58ab1a426b1a94182799e789bf183
Gento <a551.return@gmail.com>
2024-09-13 16:11:42 +0900

    sgin inとupのpageを用意

---
+++ lib/views/pages/sign_in_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+final _isInProgressProvider = StateProvider.autoDispose((ref) {
+  return false;
+});
---
+++ lib/views/pages/sign_up_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
