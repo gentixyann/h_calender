09867e34225bb6fe2dff0f9a214a3cb7050b27e4
Gento <a551.return@gmail.com>
2024-10-05 19:10:05 +0900

    firebase初期化のコードmain.dartに追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:h_calender/firebase_options.dart';
 import 'package:h_calender/router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:h_calender/theme.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  MyApp.run();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await initializeDateFormatting('ja_JP').then((_) {
+    runApp(const ProviderScope(
+      child: MyApp(),
+    ));
+  });
 }
 
 class MyApp extends ConsumerWidget {
@@ -20,14 +30,5 @@ class MyApp extends ConsumerWidget {
       routerConfig: router,
       theme: AppTheme.lightTheme,
     );
-  }
-
-  static Future<void> run() async {
-    await initializeDateFormatting('ja_JP').then((_) {
-      WidgetsFlutterBinding.ensureInitialized();
-      runApp(const ProviderScope(
-        child: MyApp(),
-      ));
-    });
   }
 }
