529f85cf27adef0811314db7ba4bd50c735984aa
Gento <a551.return@gmail.com>
2024-11-06 13:37:49 +0900

    フォーカスを外す処理をday pageで追加

--- lib/views/pages/day_page.dart
+++ lib/views/pages/day_page.dart
@@ -3,18 +3,75 @@ import 'package:h_calender/router.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:h_calender/views/pages/widgets/un_focus.dart';
 
 class DayPage extends ConsumerWidget {
   const DayPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Day Page'),
+    return UnFocus(
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('記録しよう'),
+          ),
+          body: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            children: const [
+              _TitleField(),
+              SizedBox(height: 24),
+              _MemoField(),
+              SizedBox(height: 24),
+              _SubmitButton(),
+            ],
+          )),
+    );
+  }
+}
+
+class _TitleField extends ConsumerWidget {
+  const _TitleField();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const TextField(
+      decoration: InputDecoration(
+        labelText: 'タイトル',
+        prefixIcon: Icon(Icons.label),
       ),
-      body: const Center(
-        child: Text('dayです'),
+    );
+  }
+}
+
+class _MemoField extends ConsumerWidget {
+  const _MemoField();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const TextField(
+      maxLines: null,
+      maxLength: 500,
+      keyboardType: TextInputType.multiline,
+      decoration: InputDecoration(
+        labelText: 'メモ',
+        prefixIcon: Icon(Icons.description),
+      ),
+    );
+  }
+}
+
+class _SubmitButton extends ConsumerWidget {
+  const _SubmitButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FilledButton(
+      onPressed: () => print('送信'),
+      child: const FittedBox(
+        child: Text(
+          '記録する',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
