a051076e69363a633ca7f30eec5ed8792ba1bee9
Gento <a551.return@gmail.com>
2024-11-09 18:45:06 +0900

    addEventを仮作成

--- lib/models/calendar.dart
+++ lib/models/calendar.dart
@@ -1,5 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:h_calender/models/firebase.dart';
 
 final selectedDateProvider = StateProvider<DateTime>((ref) => DateTime.now());
 
 final focusedDateProvider = StateProvider<DateTime>((ref) => DateTime.now());
+
+final addEventProvider = Provider((ref) => ({
+      required String uid,
+      required String title,
+      String? memo,
+    }) async {
+      final firestore = ref.watch(firestoreProvider);
+      await firestore.collection('users/$uid/events/').add({
+        'title': title,
+        'memo': memo,
+      });
+    });
